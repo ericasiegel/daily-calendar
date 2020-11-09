4c731d269f8db5aa2282db558933829d9a26e033
Erica Siegel <siegel.erica@gmail.com>
2020-11-09 12:12:16 -0800

    added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,23 +4,27 @@ let currentHour;
 let hourText = $(".text");
 let tasks;
 
+// set date and time
 let todaysDate = function() {
     let date = moment().format('MMMM Do YYYY, h:mm:ss a');
     let $todaysDate = $("#currentDay");
     $todaysDate.text(date);
 };
 
+// set interval for time clock seconds count
 $(document).ready(function() {
     todaysDate();
     setInterval(todaysDate, 100);
 });
 
+// get saved tasks from local storage
 if (localStorage.getItem("tasks")) {
     tasks = JSON.parse(localStorage.getItem("tasks"));
 } else {
     tasks = []
 }
 
+// place the loaded tasks in their hourly spots
 let dayTask = function() {
     $.each(tasks, function(i) {
         if (tasks[i]) {
@@ -29,6 +33,8 @@ let dayTask = function() {
     })
 };
 
+
+// create the tasks using text entered into the <textare>
 let setTasks = function() {
     var tasks = {};
     $(".text").each(function() {
@@ -38,9 +44,9 @@ let setTasks = function() {
 
 };
 
+// update the tasks by adding colors to indicate past, present, or future
 let hourlyTaskUpdate = function() {
     
-
     $(hourText).removeClass("past present future");
 
     $.each(hourText, function(scheduleHour) {
@@ -57,6 +63,7 @@ let hourlyTaskUpdate = function() {
     currentHour = dayHour.hour();
 };
 
+// set interval count for hourly tasks
 setInterval(function () {
     dayHour = moment();
     if (currentHour < dayHour.hour()) {
